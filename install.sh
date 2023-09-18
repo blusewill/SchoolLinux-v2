ec192fb8a5594253fbd53ad0f98dd3189c64f748
blusewill <blusewillstudio@protonmail.com>
2023-09-19 00:27:35 +0800

    Install Zettlr and remove some useless firefox-esr packages

--- install.sh
+++ install.sh
@@ -27,7 +27,10 @@ mkdir -p /home/$username/.fonts
 chown -R $username:$username /home/$username
 
 # Installing Essential Programs 
-nala install kde-plasma-desktop libreoffice libreoffice-gtk3 lsb-release firefox-esr* flatpak stellarium stellarium-data kdenlive kdenlive-data mousepad sddm vim extremetuxracer extremetuxracer-data ttf-mscorefonts-installer dolphin python3.11-venv python3.11 ark -y
+nala install kde-plasma-desktop libreoffice libreoffice-gtk3 lsb-release firefox-esr firefox-esr-l10n* flatpak stellarium stellarium-data kdenlive kdenlive-data mousepad sddm vim extremetuxracer extremetuxracer-data ttf-mscorefonts-installer dolphin python3.11-venv python3.11 ark -y
+
+flatpak install flathub com.zettlr.Zettlr -y
+
 # Installing Other less important Programs
 nala install mixxx mixxx-data lmms lmms-common -y
 
