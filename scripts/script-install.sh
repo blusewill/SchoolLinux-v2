e36042b79fc1b1edbed1c54abc2e827540210dc0
blusewill <blusewillstudio@protonmail.com>
2023-09-20 18:25:48 +0800

    Update Script installation method and remove firefox package in Essential settings

---
+++ scripts/script-install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Checking if is running in Repo Folder
+if [[ "$(basename "$(pwd)" | tr '[:upper:]' '[:lower:]')" =~ ^scripts$ ]]; then
+    echo "You are running this in Schoollinux-v2 Folder."
+    echo "Please use ./install.sh instead"
+    exit
+fi
+
+# Installing git
+
+echo "Installing git."
+apt install -y git
+
+echo "Cloning the School Linux Project"
+git clone https://github.com/blusewill/Schoollinux-v2
+
+echo "Executing School Linux Script"
+
+cd $HOME/Schoollinux-v2
+
+sudo ./install.sh
